7423873b8252dd2d7bd1a88d904f09cdf40483c3
Nunisey <3128375965@qq.com>
2024-05-27 11:03:19 +0800

    commit B2

--- src/Main.java
+++ src/Main.java
@@ -29,6 +29,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         String myName = "Nunisey";
+        String msg = "B2";
         String fileName = "input.txt";
         List<String> words = readWordsFromFile(fileName);
         Graph = buildGraph(words);
